2487273f903413878e585d644ff60e2efd7c4b2f
Grandapk <Grandapk@gmail.com>
2025-02-25 20:35:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -78,3 +78,13 @@ document.addEventListener("DOMContentLoaded", function () {
         contactForm.classList.toggle("show");
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const faqItems = document.querySelectorAll(".faq-item");
+
+    faqItems.forEach((item) => {
+        item.querySelector(".faq-question").addEventListener("click", function () {
+            item.classList.toggle("active");
+        });
+    });
+});
